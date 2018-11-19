4018b78cafca7e01c835f57f19237b23b6c8788e
Michał Moskal <michal.moskal.55@gmail.com>
2018-11-20 00:59:52 +0100

    World Object (for future camera movement) and Platform class

--- main.js
+++ main.js
@@ -74,7 +74,7 @@ class Assets {
 			}
 		}
 	}
-};
+}
 
 class Player {
 	constructor(){
@@ -182,10 +182,75 @@ class Player {
 		ctx.arc(this.x, this.y, 10, 0, 2*PI);
 		ctx.stroke();
 	}
-};
+}
+
+class Platform {
+	constructor(){
+		this.x = 500;
+		this.y = 500;
+		this.length = Math.ceil(Math.random() * 300) + 200;
+	}
+
+	render(){
+		// Draw position Circle
+		ctx.beginPath();
+		ctx.arc(this.x, this.y, 5, 0, 2*PI);
+		ctx.stroke();
+
+		// Draw length line
+		ctx.lineWidth = 0.5;
+		ctx.beginPath();
+		ctx.moveTo(this.x, this.y);
+		ctx.lineTo(this.x + this.length, this.y);
+		ctx.stroke();
+
+		let t_left = ASSETS.get('tile_LEFT');
+		let t_mid = ASSETS.get('tile_MID');
+		let t_right = ASSETS.get('tile_RIGHT');
+
+		// Draw middle
+		let mid_width = this.length - t_left.width - t_right.width;
+		let times_fit = mid_width / t_mid.width;
+		let repeat = Math.ceil(times_fit);
+		let ratio = times_fit / repeat;
+
+		for(let i=0; i<repeat; i++){
+			ctx.drawImage(t_mid, this.x + t_left.width + (i * t_mid.width * ratio), this.y, t_mid.width * ratio + 1, t_mid.height);
+		}
+
+		// Draw corners
+		ctx.drawImage(t_left, this.x, this.y);
+		ctx.drawImage(t_right, this.x + this.length - 70, this.y);
+	}
+}
+
+class World {
+	constructor(){
+		this.x = 0;
+		this.y = 0;
+
+		this.objects = [];
+
+		let wo = new Platform();
+		this.objects.push(wo);
+	}
+
+	update(deltaTime){
+		// this.objects.forEach((o) => {
+		// 	o.update(deltaTime);
+		// });
+	}
+
+	render(){
+		this.objects.forEach((o) => {
+			o.render();
+		});
+	}
+}
 
 var PLAYER;
 var ASSETS;
+var WORLD;
 var KEYS = {};
 
 function setup(){
@@ -215,11 +280,15 @@ function setup(){
 	ASSETS.load();
 
 	PLAYER = new Player();
+	WORLD = new World();
 }
 
 function update(deltaTime){
 	// Player
 	PLAYER.update(deltaTime);
+
+	// Level
+	WORLD.update(deltaTime);
 }
 
 function render(){
@@ -229,6 +298,9 @@ function render(){
 	ctx.globalAlpha = 0.5;
 	ctx.drawImage(ASSETS.get('BG'), 0, 0);
 	ctx.globalAlpha = 1;
+
+	// Level
+	WORLD.render();
 
 	// Line
 	ctx.beginPath();
